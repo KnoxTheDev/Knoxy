6044f56a8986d4b54529fccc23e952700d14d7d4
KnoxTheDev <tuxltuxl001@gmail.com>
2024-09-03 13:27:49 +0530

    Initial Commit

---
+++ src/main/java/knoxy/knoxy/KnoxyDataGenerator.java
@@ -0,0 +1,11 @@
+package knoxy.knoxy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KnoxyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
