eb915110a6818ed5a1d65c51920ceced2f0418ce
Knox <tuxltuxl001@gmail.com>
2024-09-03 17:35:59 +0530

    Update Knoxy.java

--- src/main/java/knoxy/knoxy/Knoxy.java
+++ src/main/java/knoxy/knoxy/Knoxy.java
@@ -1,41 +1,50 @@
 package knoxy.knoxy;
 
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.Packet;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.util.math.Vec3d;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.util.Identifier;
 
-public class Knoxy {
+public class Knoxy implements ClientModInitializer, ModInitializer {
+    private static final Identifier PACKET_ID = new Identifier("knoxy", "fall_damage_knockback");
 
-    @Environment(EnvType.CLIENT)
-    public void onEnable() {
-        // Get the Minecraft client instance
-        MinecraftClient client = MinecraftClient.getInstance();
-
-        // Get the client play network handler
-        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
-
-        // Create a new thread to send the packets
-        new Thread(() -> {
-            while (true) {
-                // Check if the player is falling
-                if (client.player.isFallFlying() || client.player.getVelocity().y < 0) {
-                    // Send the "OnGround" packet to the server
-                    Packet<?> packet = new PlayerMoveC2SPacket.OnGroundPacket(true);
-                    networkHandler.sendPacket(packet);
-                }
-
-                // Sleep for a short period of time to avoid spamming the server
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void onInitializeClient() {
+        // Client-side: Disable fall damage using simple methods
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            PlayerEntity player = MinecraftClient.getInstance().player;
+            if (player != null && player.fallDistance > 0) {
+                player.fallDistance = 0;
+                // Send packet to server to update onGround status
+                PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+                buf.writeBoolean(true);
+                ClientPlayNetworking.send(PACKET_ID, buf);
             }
-        }).start();
+        });
+
+        // Client-side: Listen for packets to set onGround status
+        ClientPlayNetworking.registerGlobalReceiver(PACKET_ID, (client, handler, buf, responseSender) -> {
+            if (client.player != null) {
+                client.execute(() -> {
+                    client.player.setOnGround(buf.readBoolean());
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onInitialize() {
+        // Server-side: Listen for packets to set onGround status
+        ServerPlayNetworking.registerGlobalReceiver(PACKET_ID, (server, player, handler, buf, responseSender) -> {
+            boolean onGround = buf.readBoolean();
+            server.execute(() -> {
+                player.setOnGround(onGround);
+            });
+        });
     }
 }
